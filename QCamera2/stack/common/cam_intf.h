c1de539bd8af5b058a331648ad6724c0c3f3a332
dianlujitao <dianlujitao@lineageos.org>
2019-09-29 13:55:05 +0200

    bach:camera add CAM_INTF_META_HUAWEI_00 to metadata_data_t

--- QCamera2/stack/common/cam_intf.h
+++ QCamera2/stack/common/cam_intf.h
@@ -1014,7 +1014,7 @@ typedef struct {
     INCLUDE(CAM_INTF_PARM_MANUAL_CAPTURE_TYPE,          cam_manual_capture_type,     1);
     INCLUDE(CAM_INTF_AF_STATE_TRANSITION,               uint8_t,                     1);
     INCLUDE(CAM_INTF_PARM_INITIAL_EXPOSURE_INDEX,       uint32_t,                    1);
-    volatile char huawei_reserved04[20];
+    INCLUDE(CAM_INTF_META_HUAWEI_00,               	uint8_t,                     20);
     INCLUDE(CAM_INTF_META_FOCUS_DEPTH_INFO,             uint8_t,                     8);
     INCLUDE(CAM_INTF_PARM_INSTANT_AEC,                  uint8_t,                     8);
     INCLUDE(CAM_INTF_META_REPROCESS_FLAGS,              uint8_t,                     1);
